a5a2fa8d560fa24a5d668c54e7147627d5c9b2a7
oculante <151761418+oculante@users.noreply.github.com>
2024-03-11 22:51:24 +0100

    Update ScoreManager.java

--- src/Lab3/ScoreManager.java
+++ src/Lab3/ScoreManager.java
@@ -2,40 +2,37 @@ package Lab3;
 
 import java.io.Serializable;
 import java.util.*;
+import javax.swing.*;
 
 public class ScoreManager implements Serializable {
     private static final long serialVersionUID = 1L;
     private int currentRunNumber = 1;
-    private List<Integer> latestRuns = new ArrayList<>();
-    private List<Integer> latestRunsScores = new ArrayList<>();
-    private List<Integer> highestScores = new ArrayList<>();
-    private List<Integer> highestScoresRuns = new ArrayList<>();
-    private List<Integer> latestScores = new ArrayList<>();
+    private DefaultListModel<Integer> latestRuns = new DefaultListModel<>();
+    private DefaultListModel<Integer> latestRunsScores = new DefaultListModel<>();
+    private DefaultListModel<Integer> highestScores = new DefaultListModel<>();
+    private DefaultListModel<Integer> latestScores = new DefaultListModel<>();
     private Map<Integer, String> highestScoresNames = new HashMap<>();
     private static final int MAX_LIST_SIZE = 10;
 
     public void addScore(int score, String playerName) {
-        latestRuns.add(currentRunNumber);
-        latestRunsScores.add(score);
+        latestRuns.addElement(currentRunNumber);
+        latestRunsScores.addElement(score);
         currentRunNumber++;
 
-        if (highestScores.size() < MAX_LIST_SIZE) {
+        if (highestScores.getSize() < MAX_LIST_SIZE) {
             // If the highest scores list is not full, simply add the new score
-            highestScores.add(score);
-            highestScoresRuns.add(currentRunNumber - 1);
+            highestScores.addElement(score);
             highestScoresNames.put(score, playerName); // Associate the name with the score
             sortScoresDescending();
         } else {
             // If the highest scores list is full, check if the new score qualifies to be added
-            if (score > highestScores.get(highestScores.size() - 1)) {
+            if (score > highestScores.elementAt(highestScores.getSize() - 1)) {
                 // Remove the lowest score
-                highestScores.remove(highestScores.size() - 1);
-                highestScoresRuns.remove(highestScoresRuns.size() - 1);
-                highestScoresNames.remove(highestScores.get(highestScores.size() - 1));
+                highestScores.removeElementAt(highestScores.getSize() - 1);
+                highestScoresNames.remove(highestScores.elementAt(highestScores.getSize() - 1));
 
                 // Add the new score
-                highestScores.add(score);
-                highestScoresRuns.add(currentRunNumber - 1);
+                highestScores.addElement(score);
                 highestScoresNames.put(score, playerName); // Associate the name with the score
                 sortScoresDescending();
             }
@@ -43,62 +40,56 @@ public class ScoreManager implements Serializable {
 
         limitListSize(latestRuns, MAX_LIST_SIZE);
         limitListSize(latestRunsScores, MAX_LIST_SIZE);
-        latestScores.add(score);
+        latestScores.addElement(score);
         limitListSize(latestScores, MAX_LIST_SIZE);
     }
 
 
-    public List<Integer> getHighestScores() {
-        return highestScores;
+
+    public JList<Integer> getHighestScores() {
+        return new JList<>(highestScores);
     }
 
-    public List<Integer> getHighestScoresRuns() {
-        return highestScoresRuns;
+
+    public JList<Integer> getLatestRuns() {
+        return new JList<>(latestRuns);
     }
 
-    public List<Integer> getLatestRuns() {
-        return latestRuns;
-    }
-
-    public List<Integer> getLatestScores() {
-        return latestScores;
+    public JList<Integer> getLatestScores() {
+        return new JList<>(latestScores);
     }
 
     public Map<Integer, String> getHighestScoresNames() {
         return highestScoresNames;
     }
 
-    private void limitListSize(List<Integer> list, int maxSize) {
-        while (list.size() > maxSize) {
-            list.remove(0);
+    private void limitListSize(DefaultListModel<Integer> listModel, int maxSize) {
+        while (listModel.getSize() > maxSize) {
+            listModel.remove(0);
         }
     }
 
     private void sortScoresDescending() {
         ArrayList<ScoreRunPair> pairs = new ArrayList<>();
-        for (int i = 0; i < highestScores.size(); i++) {
-            pairs.add(new ScoreRunPair(highestScores.get(i), highestScoresRuns.get(i)));
+        for (int i = 0; i < highestScores.getSize(); i++) {
+            pairs.add(new ScoreRunPair(highestScores.getElementAt(i)));
         }
         Collections.sort(pairs);
         Collections.reverse(pairs);
 
-        highestScores.clear();
-        highestScoresRuns.clear();
+        highestScores.removeAllElements();
 
         for (ScoreRunPair pair : pairs) {
-            highestScores.add(pair.score);
-            highestScoresRuns.add(pair.run);
+            highestScores.addElement(pair.score);
         }
     }
 
     private class ScoreRunPair implements Comparable<ScoreRunPair>, Serializable {
         private static final long serialVersionUID = 1L;
         private int score;
-        private int run;
 
-        public ScoreRunPair(int score, int run) {
+        public ScoreRunPair(int score) {
             this.score = score;
-            this.run = run;
         }
 
         @Override
